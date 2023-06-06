ba366e1feec5a8981c2e85465d88df750ea51088
Artem Moskvin <artemm@spotify.com>
2023-06-06 18:17:56 +0200

    Add translation

--- fluently/app.py
+++ fluently/app.py
@@ -3,19 +3,24 @@ import os
 import openai
 from dotenv import load_dotenv
 from flask import abort, Flask, request, jsonify
+from langchain.chat_models import ChatOpenAI
 
 from fluently.completion import get_completion
+from fluently.translation.openai_translator import OpenAITranslator
 
 load_dotenv()
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+chat_llm = ChatOpenAI()
+openai_translator = OpenAITranslator(chat_llm)
 
 app = Flask(__name__)
 
 
 @app.route('/complete', methods=['POST'])
 def complete():
-    app.logger.info("Got request from the client!")
+    app.logger.info("Received completion request")
 
     text = request.json.get("text", None)
 
@@ -28,7 +33,28 @@ def complete():
 
             return jsonify({"completion": completion})
         except Exception as e:
-            app.logger.error(f"Cannot create completion: {e}", exc_info=True)
+            app.logger.error(f"Completion failed: {e}", exc_info=True)
             abort(500, description="Completion failed")
+
+    abort(400, description="Text is empty")
+
+
+@app.route("/translate", methods=["POST"])
+def translate():
+    app.logger.info("Received translation request")
+
+    text = request.json.get("text", None)
+
+    if text:
+        app.logger.info("Text: " + text)
+        try:
+            translation = openai_translator.translate(text)
+
+            app.logger.info("Translation: " + translation)
+
+            return jsonify({"translation": translation})
+        except Exception as e:
+            app.logger.error(f"Translation failed: {e}", exc_info=True)
+            abort(500, description="Translation failed")
 
     abort(400, description="Text is empty")
--- tests/test_app.py
+++ tests/test_app.py
@@ -25,8 +25,37 @@ def test_complete_fails(mock_get_completion):
     mock_get_completion.assert_called_once_with("I am")
 
 
-def test_text_is_empty():
+def test_complete_text_is_empty():
     with app.test_client() as client:
         response = client.post("/complete", json={})
+    assert response.status_code == 400
+    assert "Text is empty" in response.text
+
+
+@patch('fluently.app.openai_translator.translate', return_value="Hello!")
+def test_translate_succeeds(mock_get_completion):
+    with app.test_client() as client:
+        response = client.post("/translate", json={
+            "text": "Привет!"
+        })
+    assert response.status_code == 200
+    assert response.json["translation"] == "Hello!"
+    mock_get_completion.assert_called_once_with("Привет!")
+
+
+@patch('fluently.app.openai_translator.translate', side_effect=Exception("Test"))
+def test_translate_fails(mock_get_completion):
+    with app.test_client() as client:
+        response = client.post("/translate", json={
+            "text": "Привет!"
+        })
+    assert response.status_code == 500
+    assert "Translation failed" in response.text
+    mock_get_completion.assert_called_once_with("Привет!")
+
+
+def test_translate_text_is_empty():
+    with app.test_client() as client:
+        response = client.post("/translate", json={})
     assert response.status_code == 400
     assert "Text is empty" in response.text
