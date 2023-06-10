f2369916d593c309d1f9dcd77b82252693925f46
Artem Moskvin <artemm@spotify.com>
2023-06-10 16:35:20 +0200

    Add Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'fluently:create_app()'
--- fluently/__init__.py
+++ fluently/__init__.py
@@ -17,7 +17,7 @@ def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
     )
 
     if test_config is None:
